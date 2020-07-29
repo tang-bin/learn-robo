ee94138884c9a8c6aea099056e4d8906a45c0551
Tang, Bin <77304+tang-bin@users.noreply.github.com>
2020-07-28 19:16:38 -0700

    update

--- home_robot/test_slam.sh
+++ home_robot/test_slam.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-x-terminal-emulator -e " gazebo " &
+x-terminal-emulator -e " source devel/setup.zsh" &
+sleep 1
+x-terminal-emulator -e " roslaunch my_robot world.launch" &
 sleep 5
-x-terminal-emulator -e " source /opt/ros/noetic/setup.zsh; roscore" &
+x-terminal-emulator -e " roslaunch my_robot mapping.launch" &
 sleep 5
-x-terminal-emulator -e " rosrun rviz rviz"
+x-terminal-emulator -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+x-terminal-emulator -e " roslaunch my_robot amcl.launch" &
+sleep 1
+x-terminal-emulator -e " rosrun teleop_twist_keyboard teleop_twist_keyboard.py" &
