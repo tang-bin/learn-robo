53c0213f560c087cd810d62d4c315ffc543e3271
Tang, Bin <77304+tang-bin@users.noreply.github.com>
2020-08-04 08:48:58 -0700

    update

--- home_robot/test_nav.sh
+++ home_robot/test_nav.sh
@@ -2,10 +2,10 @@
 x-terminal-emulator -e " source devel/setup.zsh" &
 sleep 1
 x-terminal-emulator -e " roslaunch my_robot world.launch" &
-sleep 1
-x-terminal-emulator -e " roslaunch my_robot mapping.launch" &
-sleep 1
+sleep 5
 x-terminal-emulator -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+x-terminal-emulator -e " roslaunch my_robot amcl.launch" &
 sleep 1
 x-terminal-emulator -e " rosrun teleop_twist_keyboard teleop_twist_keyboard.py _speed:=0.5 _turn:=0.5" &
 # x-terminal-emulator -e " roslaunch my_robot teleop.launch" &
